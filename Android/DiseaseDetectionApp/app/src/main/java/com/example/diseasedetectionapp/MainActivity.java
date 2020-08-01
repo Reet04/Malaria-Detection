38f59bc6fd3c6ada5bc14e6342f1d498b1931eb8
Dhruv <dhruv_devasthale@outlook.com>
2020-08-01 18:52:30 +0530

    Python integration

--- Android/DiseaseDetectionApp/app/src/main/java/com/example/diseasedetectionapp/MainActivity.java
+++ Android/DiseaseDetectionApp/app/src/main/java/com/example/diseasedetectionapp/MainActivity.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView1, imageView2, imageView3, imageView4;
@@ -23,12 +28,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(!Python.isStarted()){
+            Python.start(new AndroidPlatform(MainActivity.this));
+        }
+
         button = (Button) findViewById(R.id.chooseImgBtn);
         imageView1 = (ImageView) findViewById(R.id.selectImg1);
         imageView2 = (ImageView) findViewById(R.id.selectImg2);
         imageView3 = (ImageView) findViewById(R.id.selectImg3);
         imageView4 = (ImageView) findViewById(R.id.selectImg4);
         progressBar=findViewById(R.id.progressBar);
+        executePythonScript();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
                     selectIndex++;
 
                 }
-
                 openGallery();
             }
         });
@@ -45,6 +54,11 @@ public class MainActivity extends AppCompatActivity {
     private void openGallery() {
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE);
+    }
+    public void executePythonScript(){
+        Python python=Python.getInstance();
+        PyObject test=python.getModule("test");
+        Log.i("PYTHON:",test.callAttr("test").toString());
     }
 
     @Override
